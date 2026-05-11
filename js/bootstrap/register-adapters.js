561e734baec16bdd2f06af6b2dac1c9e689b074a
chemagimenez-byte <chema.gimenez@gmail.com>
2026-05-11 11:28:36 +0200

    Update register-adapters.js

--- js/bootstrap/register-adapters.js
+++ js/bootstrap/register-adapters.js
@@ -6,20 +6,21 @@
   }
 
   if (window.MultiTraceRiseAdapter) {
-    registry.register(window.MultiTraceRiseAdapter);
+    registry.register(new window.MultiTraceRiseAdapter());
   }
 
   if (window.MultiTraceStorylineAdapter) {
-    registry.register(window.MultiTraceStorylineAdapter);
+    registry.register(new window.MultiTraceStorylineAdapter());
   }
 
   // NUEVO: registrar adaptador de eXeLearning
   if (window.MultiTraceExeLearningAdapter) {
-    registry.register(window.MultiTraceExeLearningAdapter);
+    registry.register(new window.MultiTraceExeLearningAdapter());
   }
 
-  // NUEVO: registrar adaptador de LLM (Debe ir el último, es la red de seguridad)
+  // NUEVO: registrar adaptador para SCORMs de LLM
   if (window.MultiTraceLLMAdapter) {
-    registry.register(window.MultiTraceLLMAdapter);
+    console.log("[DEBUG] Registrando adaptador LLM...");
+    registry.register(new window.MultiTraceLLMAdapter());
   }
 })();
